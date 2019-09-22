d9a73483dcb502602826ec6ac9f203c448fcc0e0
Christopher Ruaboro <Christopher.Ruaboro@outlook.com>
2019-09-21 21:16:47 -0700

    Add project files.

---
+++ NCO/Models/ApplicationsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NCO.Models
+{
+    public class ApplicationsDbContext : DbContext
+    {
+        public ApplicationsDbContext(DbContextOptions<ApplicationsDbContext> options)
+            : base(options) { }
+
+        public DbSet<ReviewModel> ReviewModels { get; set; }
+    }
+}
---
+++ NCO/Models/Units.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NCO.Models
+{
+    public class Units
+    {
+        [Key]
+        public int UnitIDCode { get; set; }
+        public string UnitName { get; set; }
+        public int ParentTo { get; set; }
+        public int ChildTo { get; set; }
+        public int POBox { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+        public int Phone { get; set; }
+    }
+}
